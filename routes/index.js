1b3554bcf0d184f727211029296a6c4444c32d50
Jan Florian Dietrich <jfd@daenen4.de>
2015-05-03 12:47:55 +0200

    * Finished initial application

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,134 @@
 var express = require('express');
 var router = express.Router();
 
+require('../models/Posts.js');
+require('../models/Comments.js');
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+/**
+ * Middleware for getting a post by its id
+ */
+router.param('post', function (req, res, next, id) {
+    var query = Post.findById(id);
+
+    query.exec(function (err, post) {
+        if (err) {
+            return next(err);
+        }
+
+        if (!post) {
+            return next(new Error("Can't find post"));
+        }
+
+        req.post = post;
+        return next();
+    });
+});
+
+/**
+ * Middleware for getting a comment by its id
+ */
+router.param('comment', function (req, res, next, id) {
+    var query = Comment.findById(id);
+
+    query.exec(function (err, comment) {
+        if (err) {
+            return next(err);
+        }
+
+        if (!comment) {
+            return next(err);
+        }
+
+        req.comment = comment;
+        return next();
+    });
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get(
+    '/', function (req, res, next) {
+        res.render('index', {title : 'Express'});
+    }
+);
+
+/* GET Posts Rest Response */
+router.get('/posts', function (req, res, next) {
+    Post.find(function (err, posts) {
+        if (err) {
+            return next(err);
+        }
+
+        res.json(posts);
+    });
+});
+
+/* POST Save a post object */
+router.post('/posts', function(req, res, next) {
+    var post = new Post(req.body);
+
+    post.save(function (err, post) {
+        if (err) {
+            return next(err);
+        }
+
+        res.json(post);
+    });
+});
+
+/* GET single post */
+router.get('/posts/:post', function (req, res) {
+    req.post.populate('comments', function (err, post) {
+        if (err) {
+            return next(err);
+        }
+
+        res.json(post)
+    });
+});
+
+/* PUT upvote a post */
+router.put('/posts/:post/upvote', function (req, res, next) {
+    req.post.upvote(function(err, post) {
+        if (err) {
+            return next(err);
+        }
+
+        res.json(post);
+    });
+});
+
+/* POST atach comment to post */
+router.post('/posts/:post/comments', function (req, res, next) {
+    var comment = new Comment(req.body);
+    comment.post = req.post;
+
+    comment.save(function(err, comment) {
+        if (err) {
+            return next(err);
+        }
+
+        req.post.comments.push(comment);
+        req.post.save(function(err, post) {
+            if (err) {
+                return next(err);
+            }
+
+            res.json(comment);
+        });
+    });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
+    req.comment.upvote(function(err, comment) {
+        if (err) {
+            return (next);
+        }
+
+        res.json(comment);
+    });
 });
 
 module.exports = router;
