a692efca349bcedb31db611e748c26e2e4ac03af
Jan Florian Dietrich <jfd@daenen4.de>
2015-05-04 13:28:02 +0200

    * Added authentication via passport

---
+++ README.md
@@ -0,0 +1,5 @@
+### Flapper News
+
+This is the implementation of the tutorial at https://thinkster.io/mean-stack-tutorial
+
+`npm start`
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var passport = require('passport');
+var jwt = require('express-jwt');
 var router = express.Router();
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 require('../models/Posts.js');
 require('../models/Comments.js');
+require('../models/Users.js');
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 /**
  * Middleware for getting a post by its id
@@ -66,8 +71,9 @@ router.get('/posts', function (req, res, next) {
 });
 
 /* POST Save a post object */
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
     var post = new Post(req.body);
+    post.author = req.payload.username;
 
     post.save(function (err, post) {
         if (err) {
@@ -90,7 +96,7 @@ router.get('/posts/:post', function (req, res) {
 });
 
 /* PUT upvote a post */
-router.put('/posts/:post/upvote', function (req, res, next) {
+router.put('/posts/:post/upvote', auth, function (req, res, next) {
     req.post.upvote(function(err, post) {
         if (err) {
             return next(err);
@@ -101,9 +107,10 @@ router.put('/posts/:post/upvote', function (req, res, next) {
 });
 
 /* POST atach comment to post */
-router.post('/posts/:post/comments', function (req, res, next) {
+router.post('/posts/:post/comments', auth, function (req, res, next) {
     var comment = new Comment(req.body);
     comment.post = req.post;
+    comment.author = req.payload.username;
 
     comment.save(function(err, comment) {
         if (err) {
@@ -121,7 +128,7 @@ router.post('/posts/:post/comments', function (req, res, next) {
     });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function (req, res, next) {
     req.comment.upvote(function(err, comment) {
         if (err) {
             return (next);
@@ -129,6 +136,42 @@ router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
 
         res.json(comment);
     });
+});
+
+router.post('/register', function (req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Please fill out all fields' });
+    }
+
+    var user = new User();
+    user.username = req.body.username;
+    user.setPassword(req.body.password);
+
+    user.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+
+        return res.json({ token: user.generateJWT() });
+    });
+});
+
+router.post('/login', function(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Please fill out all fields' });
+    }
+
+    passport.authenticate('local', function (err, user, info) {
+        if (err) {
+            return next(err);
+        }
+
+        if (user) {
+            return res.json({ token: user.generateJWT() });
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
 });
 
 module.exports = router;
